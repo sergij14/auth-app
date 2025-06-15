eb7c633ca89b1a24ef4b2407b28a81713820ce8a
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-06-15 23:10:47 +0400

    feat: added protected route & auth middleware

--- index.js
+++ index.js
@@ -86,6 +86,43 @@ app.post("/api/auth/login", async (req, res) => {
   }
 });
 
+app.get("/api/users/current", authMiddleware, async (req, res) => {
+  try {
+    const user = await users.findOne({ _id: req.user.id });
+
+    return res.status(200).json({
+      id: user._id,
+      email: user.email,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: err.message ? err.message : "Internal server error" });
+  }
+});
+
+async function authMiddleware(req, res, next) {
+  const accessToken = req.headers.authorization;
+
+  if (!accessToken) {
+    return res.status(401).json({ message: "Access token not found" });
+  }
+
+  try {
+    const decodedAccessToken = jwt.verify(
+      accessToken,
+      config.ACCESS_TOKEN_SECRET
+    );
+
+    req.user = {
+      id: decodedAccessToken.userId,
+    };
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Access token invalid or expired" });
+  }
+}
+
 app.listen(3000, () => {
   console.log("Server started...");
 });
